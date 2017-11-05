36f374137026350dac00cbba8bee19c7892c8736
JasperHub1 <31580595+JasperHub1@users.noreply.github.com>
2017-11-05 14:04:20 +1100

    re-write everything from scratch / using bootstrap 4.0

--- js/main.js
+++ js/main.js
@@ -1,4 +1,16 @@
 $(document).ready(() => {
+	$('.btn').on('click', event => {
+		event.preventDefault();
+		var emailValue = $('#email-input').val();
+		var passwordValue = $('#password-input').val();
+		
+		if (emailValue == "") {
+			$('.email-error').html("Please enter your email")
+		}
 
+		if (passwordValue == "") {
+			$('.password-error').html("Please enter your password");
+		}
+	})
 })
 
